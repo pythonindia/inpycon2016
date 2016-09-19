99fb2c7e140c0b6ea6921e5f040633d2bbfabdcb
Anuvrat Parashar <bhanuvrat@gmail.com>
2016-09-19 17:44:31 +0530

    Schedule (#98)
    * Talks arranged as per timings
    
    * Schedule changed according to the sheet shared.
    
    * Schedule changed according to file shared
    
    * .idea folder deleted
    
    * Corrects schedule according to different rooms
    
    * update schedule with speaker names
    
    * add missing tea break

--- build.py
+++ build.py
@@ -8,7 +8,32 @@ from staticjinja import make_site
 
 def get_schedule(file_path='data/schedule.json'):
     with open(file_path) as data_file:
-        return json.load(data_file)
+        schedule =  json.load(data_file)
+        i=0
+        for day in schedule:
+            events_list = []
+            day_events = day['events']
+            events_dict_in_list={}
+            events_dict_in_list['talks'] = []
+            event_time = day_events[0]['time']
+            for event in day_events:
+                if event_time == event['time']:
+                    events_dict_in_list['time'] = event['time']
+                    del event['time']
+                    events_dict_in_list['talks'].append(event.copy())
+
+                else:
+                    events_list.append(events_dict_in_list)
+                    events_dict_in_list = {}
+                    events_dict_in_list['talks'] = []
+                    events_dict_in_list['time'] = event['time']
+                    event_time = event['time']
+                    del event['time']
+                    events_dict_in_list['talks'].append(event.copy())
+            events_list.append(events_dict_in_list)
+            schedule[i]['events'] = events_list
+            i+=1
+        return schedule
 
 
 if __name__ == "__main__":
